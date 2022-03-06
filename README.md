36e6b0628357dea854d81cc24bdd46a328abdaf6
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-03-06 11:23:01 -0500

    Fix index.js; script works

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 #Scrape to SQL
 
-Scraper to pull frame data from Dustloop Website for PostgreSQL.
-I used code for the scraper from Ania Kubow's nodejs webscraper (https://github.com/kubowania/nodejs-webscraper), making alterations specific to data desired. SQL output is tricky, but will be done soon.
+Web-scraper to pull frame data from Dustloop Website for PostgreSQL.
+
+I used code for the scraper from Ania Kubow's nodejs webscraper (https://github.com/kubowania/nodejs-webscraper), and made alterations specific to data desired.
+Code works, but needs refactoring and polish!
--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const PORT = 3000;
 
 // There is definitely a better way, but this works.
 const character_links = [
-	"https://dustloop.com/wiki/index.php?title=GGST/Ramlethal_Valentine/Frame_Data", 
-	"https://dustloop.com/wiki/index.php?title=GGST/Sol_Badguy/Frame_Data",/*
+	"https://dustloop.com/wiki/index.php?title=GGST/Ramlethal_Valentine/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Sol_Badguy/Frame_Data",
 	"https://dustloop.com/wiki/index.php?title=GGST/Jack-O/Frame_Data",
 	"https://dustloop.com/wiki/index.php?title=GGST/Nagoriyuki/Frame_Data",
 	"https://dustloop.com/wiki/index.php?title=GGST/Millia_Rage/Frame_Data",
@@ -26,7 +26,7 @@ const character_links = [
 	"https://dustloop.com/wiki/index.php?title=GGST/Axl_Low/Frame_Data",
 	"https://dustloop.com/wiki/index.php?title=GGST/Potemkin/Frame_Data",
 	"https://dustloop.com/wiki/index.php?title=GGST/Giovanna/Frame_Data",
-	"https://dustloop.com/wiki/index.php?title=GGST/Goldlewis_Dickinson/Frame_Data" */
+	"https://dustloop.com/wiki/index.php?title=GGST/Goldlewis_Dickinson/Frame_Data"
 ];
 
 let character_id = 1;
@@ -41,31 +41,54 @@ character_links.forEach(link => {
 			console.log(character_name);
 
 			$(".cargoDynamicTable tr", HTML).each((_, element) => {
-				let attack = $(element).text().replace(/\s+/g, ' ').substring(1);
+				const attack = $(element).text().replace(/ +/g, "_").replace(/\s+/g, " ").substring(1);
 				if (!attack.startsWith("input") && !attack.startsWith("name")) { data_hold.push(attack) };
 			});
-			console.log(data_hold);
-			/*
-					name: character_name,
-					move_name: $(this).text(),
-					input: $(".input-badge").text() ,
-					damage: $(".attack-container "),
-					guard: $,
-					startup: $,
-					active: $,
-					recovery_frames: $,
-					on_block: $,
-					invulnerability: 
-			*/
 
 			const move_data = [];
+			data_hold.forEach(entry => {
+				const attack = entry.split(" ");
+				move_data.push(attack);
+			});
 
 			let SQL_INSERT =
 				`INSERT INTO characters VALUES (${character_id}, '${character_name.replace("_", " ")}');\n\n`;
-			/*SQL_INSERT +=
-				`INSERT INTO move_list VALUES (${move_id}, ${character_id}, ${move_name}, ${input},
-					${damage},	${guard}, ${startup}, ${active}, ${recovery_frames}, ${on_block}, ${invulnerability};\n`;
-			*/
+			
+			//This sequence sucks and should be refactored.
+			let move_id = 1;
+			move_data.forEach(attack => {					
+				let input = attack[0];
+				let move_name, damage, guard, startup, active, recovery_frames, on_block;
+				if (attack[0].includes("Throw")) {
+					move_name = attack[0];
+					damage = attack[1];
+					guard = attack[2];
+					startup = attack[3];
+					active = attack[4];
+					recovery_frames = attack[5];
+					on_block = "NULL";
+				} else {
+					if (attack[1].match(/^\d/)) {
+						move_name = attack[0];
+						damage = attack[1];
+						guard = attack[2];
+						startup = attack[3];
+						active = attack[4];
+						recovery_frames = attack[5];
+						on_block = attack[6]
+					} else {
+						move_name = attack[1];
+						damage = attack[2];
+						guard = attack[3];
+						startup = attack[4];
+						active = attack[5];
+						recovery_frames = attack[6];
+						on_block = attack[7]
+					}
+				}
+				SQL_INSERT += `INSERT INTO move_list VALUES (${move_id}, ${character_id}, '${input}', '${move_name}', '${damage}',	'${guard}', '${startup}', '${active}', '${recovery_frames}', '${on_block}');\n`;
+				move_id++;
+			});
 			character_id++;
 
 			fs.writeFile(`./sql/${character_name.toLowerCase()}.sql`, SQL_INSERT, error => {
