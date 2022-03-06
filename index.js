0df6f7fe48cd9d3f1edb6d85e4c27f9bf3f01beb
Chris Dykes <chris.t.c.dykes@gmail.com>
2022-03-05 19:04:46 -0500

    Update character links to new source

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
+// There is probably a better way, but this works.
 const character_links = [
-	"https://dustloop.com/wiki/index.php?title=GGST/Ramlethal_Valentine",
-	"https://dustloop.com/wiki/index.php?title=GGST/Sol_Badguy",
-	"https://dustloop.com/wiki/index.php?title=GGST/Jack-O",
-	"https://dustloop.com/wiki/index.php?title=GGST/Nagoriyuki",
-	"https://dustloop.com/wiki/index.php?title=GGST/Millia_Rage",
-	"https://dustloop.com/wiki/index.php?title=GGST/Chipp_Zanuff",
-	"https://dustloop.com/wiki/index.php?title=GGST/Ky_Kiske",
-	"https://dustloop.com/wiki/index.php?title=GGST/May",
-	"https://dustloop.com/wiki/index.php?title=GGST/Zato-1",
-	"https://dustloop.com/wiki/index.php?title=GGST/I-No",
-	"https://dustloop.com/wiki/index.php?title=GGST/Happy_Chaos",
-	"https://dustloop.com/wiki/index.php?title=GGST/Baiken",
-	"https://dustloop.com/wiki/index.php?title=GGST/Anji_Mito",
-	"https://dustloop.com/wiki/index.php?title=GGST/Leo_Whitefang",
-	"https://dustloop.com/wiki/index.php?title=GGST/Faust",
-	"https://dustloop.com/wiki/index.php?title=GGST/Axl_Low",
-	"https://dustloop.com/wiki/index.php?title=GGST/Potemkin",
-	"https://dustloop.com/wiki/index.php?title=GGST/Giovanna",
-	"https://dustloop.com/wiki/index.php?title=GGST/Goldlewis_Dickinson"
+	"https://dustloop.com/wiki/index.php?title=GGST/Ramlethal_Valentine/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Sol_Badguy/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Jack-O/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Nagoriyuki/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Millia_Rage/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Chipp_Zanuff/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Ky_Kiske/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/May/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Zato-1/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/I-No/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Happy_Chaos/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Baiken/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Anji_Mito/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Leo_Whitefang/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Faust/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Axl_Low/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Potemkin/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Giovanna/Frame_Data",
+	"https://dustloop.com/wiki/index.php?title=GGST/Goldlewis_Dickinson/Frame_Data"
 ];
 
 let character_id = 1;
@@ -35,26 +36,29 @@ character_links.forEach(link => {
 			const HTML = res.data;
 			const $ = cheerio.load(HTML);
 			const move_data = [];
-			const character_name = $("#fpflexsection th span").text();
+			const character_name = link.replace("https://dustloop.com/wiki/index.php?title=GGST/", "")
+				.replace("/Frame_Data", "");
+			console.log(character_name);
 
+			//$("#fpflexsection th span").text();
+			/*
 			$(".mw-headline big", HTML).each(function () {
 				move_data.push({
-					//name: character_name,
-					move_name: $(this).text(), /*
-					input: $(".input-badge").text(),
+					name: character_name,
+					move_name: $(this).text(),
+					input: $(".input-badge").text() ,
 					damage: $(".attack-container "),
 					guard: $,
 					startup: $,
 					active: $,
 					recovery_frames: $,
 					on_block: $,
-					invulnerability: $ */
+					invulnerability: $ 
 				})
-			});
-			console.log(move_data);
-
+			});*/
+			
 			let SQL_INSERT =
-				`INSERT INTO characters VALUES (${character_id}, '${character_name});\n\n`;
+				`INSERT INTO characters VALUES (${character_id}, '${character_name.replace("_", " ")}');\n\n`;
 			/*SQL_INSERT +=
 				`INSERT INTO move_list VALUES (${move_id}, ${character_id}, ${move_name}, ${input},
 					${damage},	${guard}, ${startup}, ${active}, ${recovery_frames}, ${on_block}, ${invulnerability};\n`;
